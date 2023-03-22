66ad672c08515be10ddc7fa1fd1e33b997beb47a
Shivangi1994 <45062196+Shivangi1994@users.noreply.github.com>
2023-03-22 19:17:10 +0530

    Add files via upload

---
+++ Participant/src/main/java/com/wissen/model/ResponseMessage.java
@@ -0,0 +1,12 @@
+package com.wissen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseMessage {
+    String message;
+}
